7aa212d5c88248b73d465b3f5e5e8c5372e81c3e
asvdev <57688070+asvdvl@users.noreply.github.com>
2024-04-07 05:35:37 +0300

    gpu now in use correct token length

--- bot.py
+++ bot.py
@@ -1,8 +1,13 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
+if not torch.cuda.is_available():
+    print("no cuda!")
+    exit()
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
 model_id = "sambanovasystems/SambaLingo-Russian-Chat"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", torch_dtype="auto")
 #model.eval()
 
 chat = [
@@ -10,10 +15,10 @@ chat = [
 ]
 
 def next_generate():
-    token_id = tokenizer.apply_chat_template(chat, tokenize=True, add_generation_prompt=True, return_tensors="pt")
-    print(token_id.shape)
+    token_id = tokenizer.apply_chat_template(chat, tokenize=True, add_generation_prompt=True, return_tensors="pt").to("cuda")
+    print(len(token_id[0]))
     output = model.generate(token_id, max_new_tokens=50) #,max_length=50
-    print(output.shape)
+    print(len(output[0]))
     print(tokenizer.decode(output[0],skip_special_tokens=True))
 
 next_generate()
